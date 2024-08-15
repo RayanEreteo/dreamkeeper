4ba30b5c0901cef3987702137176616c8b7a2764
RayanEreteo <rayabf5@gmail.com>
2024-08-15 21:14:01 +0200

    ajout de la fonction de register

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -2,8 +2,28 @@
 
 import { Box, Button, Flex, FormControl, FormHelperText, FormLabel, Heading, Input, Link, Text } from "@chakra-ui/react";
 import loginPageBG from "../../public/loginPageBG.jpg";
+import axios from "axios";
+import { FormEvent, useState } from "react";
 
 function Register() {
+
+  const [loading, setLoading] = useState(false)
+  const [requestData, setrequestData] = useState<any>()
+
+  async function registerUser(e: FormEvent){
+    e.preventDefault()
+    setLoading(true)
+
+    try {
+      const res = await axios.post("http://localhost:8000/newUser", {email: "rayabf5@gmail.com", password: "Nekosama123"}, {headers: {"Content-Type": "application/json"}})
+      const data = res.data
+      setrequestData(data)
+    } catch (error) {
+      //? Handle error
+    }finally{
+      setLoading(false)
+    }
+  }
 
   return (
     <main className="login">
@@ -18,20 +38,21 @@ function Register() {
       >
         <Heading>REGISTER</Heading>
         <Box className="login-container" p={6} borderRadius={10}>
-          <form>
+          <form onSubmit={registerUser}>
             <FormControl>
               <FormLabel>Email address</FormLabel>
-              <Input type="email" />
+              <Input type="email" isRequired/>
             </FormControl>
             <br />
             <FormControl>
               <FormLabel>Password</FormLabel>
-              <Input type="password" minLength={8}/>
+              <Input type="password" minLength={8} isRequired/>
             </FormControl>
             <br />
-            <Button type="submit" colorScheme="facebook" w={"100%"}>Login</Button>
+            <Button type="submit" colorScheme="facebook" w={"100%"} isLoading={loading}>Register</Button>
           </form>
           <Text textAlign={"center"} mt={6}>Already have an account ? <Link textDecoration={"underline"} href="/login">Login</Link></Text>
+          <Text textAlign={"center"} mt={3} color={requestData?.success ? "green" : "red"}>{requestData?.message}</Text>
         </Box>
       </Flex>
     </main>

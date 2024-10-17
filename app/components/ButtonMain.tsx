36206d8c6300ffac93faa3477493cc0f677f68ec
RayanEreteo <rayabf5@gmail.com>
2024-10-17 17:09:32 +0200

    modification boutton et placeholder dreamjourmal

--- app/components/ButtonMain.tsx
+++ app/components/ButtonMain.tsx
@@ -1,18 +1,20 @@
 import { Button } from "@chakra-ui/react";
+import { MouseEventHandler } from "react";
 
 interface buttonMainInterface {
   children: any;
   color?: "facebook" | "linkedin";
   isLink?: boolean;
   toWhere?: string;
-  display?: object
+  display?: object;
+  execute?: MouseEventHandler
 }
 
-function ButtonMain({ children, color, isLink = false, toWhere = '/' , display}: buttonMainInterface) {
+function ButtonMain({ children, color, isLink = false, toWhere = '/' , display, execute}: buttonMainInterface) {
   if (isLink) {
     return <a href={toWhere}><Button display={display} colorScheme={color}>{children}</Button></a>;
   } else {
-    return <Button colorScheme={color} display={display}>{children}</Button>;
+    return <Button colorScheme={color} onClick={execute} display={display}>{children}</Button>;
   }
 }
 
--- app/dreamjournal/page.tsx
+++ app/dreamjournal/page.tsx
@@ -1,8 +1,23 @@
+"use client"
+
+import { Flex, Text } from '@chakra-ui/react'
 import React from 'react'
+import ButtonMain from '../components/ButtonMain'
 
 function Dreamjournal() {
+
+  function logout() {
+    console.log("Logout");
+    
+  }
+
   return (
-    <div>page</div>
+    <main className='dream-journal'>
+      <Flex flexDirection={"column"} justifyContent={"center"} alignItems={"center"} minH={"100vh"}>
+        <Text color={"black"}>{localStorage.getItem("username")}</Text>
+        <ButtonMain isLink={false} color='linkedin' execute={logout}>Logout</ButtonMain>
+      </Flex>
+    </main>
   )
 }
 

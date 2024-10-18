062ad44a617678cc8e774cc67c0a435159dc1e9d
RayanEreteo <rayabf5@gmail.com>
2024-10-18 16:43:25 +0200

    corection de la navbar et du username

--- app/components/ContextProvider.tsx
+++ app/components/ContextProvider.tsx
@@ -4,18 +4,20 @@ import { createContext, useEffect, useState } from "react";
 // Define the context with explicit types
 export const userInfoContext = createContext({
   username: "",
-  setUsername: (username: string) => {}
+  setUsername: (username: string | undefined) => {}
 });
 
 function ContextProvider({ children }: any) {
   // Initialize with an empty string or a default value
-  const [username, setUsername] = useState<string>("");
+  const [username, setUsername] = useState<any>(undefined);
 
   // Update state after the component is mounted (client-side)
   useEffect(() => {
     const storedUsername = localStorage.getItem("username");
     if (storedUsername) {
       setUsername(storedUsername);
+    }else{
+      setUsername("")
     }
   }, []);
 

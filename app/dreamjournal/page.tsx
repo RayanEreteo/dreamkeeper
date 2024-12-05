18baeda7a765af8c9d04df79eaf7119e40295118
Ereteo Rayan <90957589+RayanEreteo@users.noreply.github.com>
2024-12-05 20:26:11 +0100

    changement du style logout

--- app/dreamjournal/page.tsx
+++ app/dreamjournal/page.tsx
@@ -1,11 +1,12 @@
 "use client"
 
-import { Button, Flex, Text } from '@chakra-ui/react'
+import { Box, Button, Flex, Text } from '@chakra-ui/react'
 import React, { useContext, useState } from 'react'
 import { useRouter } from 'next/navigation'
 import { userInfoContext } from '../components/ContextProvider'
 import axios from 'axios'
 import DreamEntry from '../components/DreamEntry'
+import { CiLogout } from 'react-icons/ci'
 
 function Dreamjournal() {
   const router = useRouter()
@@ -32,8 +33,12 @@ function Dreamjournal() {
     <main className='dream-journal'>
       <Flex flexDirection={"column"} justifyContent={"center"} alignItems={"center"} minH={"100vh"}>
         <Text color={"black"}>{"Authenticated as : " + localStorage.getItem("username")}</Text>
+        <Button onClick={logout} isLoading={loading} mb={6} bg={"black"}><CiLogout size={25}/></Button>
         <Text color={"#2274A3"} fontSize={"40px"}>Dream journal</Text>
-        <Button onClick={logout} isLoading={loading}>Logout</Button>
+        <Box id='dream-container'>
+          <DreamEntry dreamName="Lucid dream" dreamDesc="This was a lucid dream !" isLucid={true}></DreamEntry>
+          <DreamEntry dreamName="A non lucid dream" dreamDesc="This was unfortunately a non lucid dream" isLucid={false}></DreamEntry>
+        </Box>
       </Flex>
     </main>
   )

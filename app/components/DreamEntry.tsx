89ca02eef698a0744e027267e157bf0004b4532b
Ereteo Rayan <90957589+RayanEreteo@users.noreply.github.com>
2024-12-05 20:25:27 +0100

    stylisation du comp

--- app/components/DreamEntry.tsx
+++ app/components/DreamEntry.tsx
@@ -1,11 +1,40 @@
-import { Button, Flex, HStack, Text } from '@chakra-ui/react'
+import { Box, Flex, Text } from '@chakra-ui/react';
+import { ImCross } from 'react-icons/im';
 
-function DreamEntry() {
-  return (
-    <Flex>
-        <Text color={"black"}>wfdsfds</Text>
-    </Flex>
-  )
+interface DreamEntryI {
+  dreamName: string;
+  dreamDesc: string;
+  isLucid: boolean;
 }
 
-export default DreamEntry
+function DreamEntry({ dreamName, dreamDesc, isLucid }: DreamEntryI) {
+  const textColor = isLucid ? 'white' : 'black';
+
+  return (
+    <Flex flexDir={'row'} bg={isLucid ? 'blue' : 'lightblue'} mb={6} p={3} minW={'500px'}>
+      <Flex flexDir={'row'} w={'100%'} justifyContent={'space-between'}>
+        {/* Description Section */}
+        <Flex id='desc' flexDir={'column'}>
+          <Text color={textColor} fontWeight={'bolder'} mb={3}>
+            {dreamName}
+          </Text>
+          <Text color={textColor}>{dreamDesc}</Text>
+        </Flex>
+        {/* Delete Area */}
+        <Box
+          id='del-area'
+          bg={'red'}
+          display={'flex'}
+          alignItems={'center'}
+          justifyContent={'center'}
+          alignSelf={'stretch'} // Makes it take up the full height
+          minW={'50px'} // Optionally, specify a minimum width
+        >
+          <ImCross/>
+        </Box>
+      </Flex>
+    </Flex>
+  );
+}
+
+export default DreamEntry;

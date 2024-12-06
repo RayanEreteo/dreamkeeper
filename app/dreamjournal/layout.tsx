147d3679225aa9f84511b772652b895ab073a8dc
Ereteo Rayan <90957589+RayanEreteo@users.noreply.github.com>
2024-12-06 15:51:40 +0100

    Code rendu dynamic pour check si l'user est logged in

--- app/dreamjournal/layout.tsx
+++ app/dreamjournal/layout.tsx
@@ -1,8 +1,6 @@
 import { Metadata } from "next";
 import Dreamjournal from "./page";
-import axios from "axios";
-import { cookies } from "next/headers";
-import { redirect } from "next/navigation";
+import NeedAuth from "../components/NeedAuth";
 
 export const metadata: Metadata = {
   title: "Dream Keeper | Dream journal",
@@ -10,19 +8,11 @@ export const metadata: Metadata = {
 };
 
 async function DreamjournalLayout() {
-  const token = cookies().get("authToken");
-
-  try {
-    const res = await axios.post("http://localhost:8000/verifyToken", {
-      token,
-    });
-
-    if (res.status === 200) {
-      return <Dreamjournal></Dreamjournal>; 
-    }
-  } catch (error) {
-    redirect("/login")
-  }
+  return (
+    <NeedAuth>
+      <Dreamjournal></Dreamjournal>
+    </NeedAuth>
+  )
 }
 
 export default DreamjournalLayout;

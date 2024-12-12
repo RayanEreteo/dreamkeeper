e0d6b237a2c13bad2ed1891df552995b52bb59f4
Ereteo Rayan <90957589+RayanEreteo@users.noreply.github.com>
2024-12-12 17:37:37 +0100

    Stylisation de la page

--- app/components/ServerError.tsx
+++ app/components/ServerError.tsx
@@ -1,6 +1,13 @@
+import { Box, Center, Flex, Heading, VStack, Text } from "@chakra-ui/react"
+import Image from "next/image"
+
 function ServerError() {
   return (
-    <div>ServerError</div>
+    <VStack justifyContent={"center"} spacing={6} h={"100vh"}>
+        <Image src={"/server.png"} alt="server error" width={200} height={200}></Image>
+        <Heading textAlign={"center"} fontSize={"40px"} fontWeight={"bolder"}>Oops... an error occurred on my server</Heading>
+        <Text>Please refresh the page or try again later.</Text>
+    </VStack>
   )
 }
 
